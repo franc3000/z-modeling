f6837ee97801dbcd88d94071df537e2b01805033
Franklin Sarkett <franklin.sarkett@gmail.com>
2016-02-03 12:52:00 -0600

    comments

--- income_tav_ratio.R
+++ income_tav_ratio.R
@@ -14,14 +14,15 @@ setwd(this.dir)
 x = read.csv('data/income_tav_ratio.csv')
 head(x)
 
-
+# raw ratio
 hist(x$ratio, breaks=100, main='Ratio: Income / TotalAssessedValue', xlab='Ratio')
 
+# winsorized ratio
 x$ratio.winsor = winsorize(x$ratio)
 hist(x$ratio.winsor, col='gray', breaks=100, 
      main='Winsorized Ratio: Income / TotalAssessedValue', xlab='Ratio')
 
+# 0 < ratio < 95th pctile
 q95 = quantile(x$ratio, .95)
 hist(x[x$ratio.winsor < q95 & x$ratio.winsor > 0,]$ratio, breaks=60, col='gray', 
      main='0 < Ratio < 95th Pctile', xlab='Ratio')
-

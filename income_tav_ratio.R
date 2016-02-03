33ad00f172449e2ec0dea68ef1ae3ac531c4ceab
Franklin Sarkett <franklin.sarkett@gmail.com>
2016-02-03 12:47:07 -0600

    Analysis on ratio of income to total assessed value for features

---
+++ income_tav_ratio.R
@@ -0,0 +1,27 @@
+# Winsorisation is the transformation of statistics by limiting extreme values 
+# in the statistical data to reduce the effect of possibly spurious outliers.
+winsorize <- function(x) {
+  quantiles <- quantile( x, c(.05, .95 ) )
+  x[ x < quantiles[1] ] <- quantiles[1]
+  x[ x > quantiles[2] ] <- quantiles[2]
+  x
+}
+
+this.dir = '~/Documents/Code/modeling/'
+setwd(this.dir)
+
+# import the data
+x = read.csv('data/income_tav_ratio.csv')
+head(x)
+
+
+hist(x$ratio, breaks=100, main='Ratio: Income / TotalAssessedValue', xlab='Ratio')
+
+x$ratio.winsor = winsorize(x$ratio)
+hist(x$ratio.winsor, col='gray', breaks=100, 
+     main='Winsorized Ratio: Income / TotalAssessedValue', xlab='Ratio')
+
+q95 = quantile(x$ratio, .95)
+hist(x[x$ratio.winsor < q95 & x$ratio.winsor > 0,]$ratio, breaks=60, col='gray', 
+     main='0 < Ratio < 95th Pctile', xlab='Ratio')
+
